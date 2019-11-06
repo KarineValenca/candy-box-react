9b61271d89d89e33f75d80311e69abf63cc8e0db
Karine Valenca <valenca.karine@gmail.com>
2019-11-05 21:33:29 -0300

    using material ui to responsive screen

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import Header from './components/Header';
-import CandyType from './pages/CandyType'
+import CandyType from './pages/CandyType';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import './styles.css'
 
 const App = () => (
-  <div className="App">
-    <Header />
-    <CandyType />
-  </div>
+  
+  
+    <MuiThemeProvider>
+      <div className="App">
+        <Header />
+        <CandyType />
+      </div>
+    </MuiThemeProvider>
+      
+     
+  
 )
 
 export default App;
--- src/pages/CandyType/index.js
+++ src/pages/CandyType/index.js
@@ -1,18 +1,21 @@
 import React, {Component} from 'react';
 import SelectOption from '../../components/SelectOption'
+import Grid from '@material-ui/core/Grid';
 
 import './styles.css'
 
 export default class CandyType extends Component {
     render() {
         return (
-            <div className="main">
+            <div className="root">
                 <h1>Passo 1: Escolha seu doce</h1>
-                <div className="container">
-                    <SelectOption/ >
-                    <SelectOption/ >
-                    <SelectOption/ >
-                </div>
+                <Grid container spacing={1}>
+                    <Grid container item xs={12} justify="center" spacing={3}>
+                        <SelectOption/ >
+                        <SelectOption/ >
+                        <SelectOption/ >
+                    </Grid>
+                </Grid>
             </div>
         )
     }
